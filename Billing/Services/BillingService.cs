bb121625e9da3277aae096e633d46f907e307992
hak.dok0@gmail.com <hak.dok0@gmail.com>
2022-11-16 22:36:28 +0300

    Добавьте файлы проекта.

---
+++ Billing/Services/BillingService.cs
@@ -0,0 +1,14 @@
+﻿namespace Billing.Services
+{
+    public class BillingService : Billing.BillingBase
+    {
+        private readonly ILogger<BillingService> _logger;
+        public BillingService(ILogger<BillingService> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+    }
+}
